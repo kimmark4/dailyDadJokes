d9702d8bde403cbf3feeec6e7764f3951a4b788d
Felicia <nemet.felicia@gmail.com>
2022-01-22 15:38:54 -0500

    added styling to js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header.js';
 import ApiCall from './utils/ApiCall.js';
 import Results from './components/Results.js'
 import UserInputs from './components/UserInputs.js';
-
+import './styles/sass/index.scss';
 
 
 function App() {
