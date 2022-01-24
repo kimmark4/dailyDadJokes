9d4e3b7c4bf4da58f520736042115965011f4286
Felicia <nemet.felicia@gmail.com>
2022-01-24 18:10:06 -0500

    frankensteined the previos commit with lates commit

--- src/components/UserInputs.js
+++ src/components/UserInputs.js
@@ -1,29 +1,31 @@
-
+import Results from "./Results.js";
 import { useEffect, useState } from 'react'
-
-import { Link, useNavigate } from "react-router-dom";
-
+import axios from 'axios'
 
 
-const UserInputs = ({ propTerm, propLimit, propSubmit }) => {
+const UserInputs = () => {
 
-    const [userLimitChoice, setuserLimitChoice] = useState(10);
     const [userChoice, setUserChoice] = useState("placeholder");
+    const [jokes, setJokes] = useState([]);
+    const [apiError, setApiError] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [photos, setPhotos] = useState([]);
     const [submit, setSubmit] = useState(false);
-    const [usersDadJokes, setUsersDadJokes] = useState(["asdf"]);
-    const navigate = useNavigate();
+    const [userLimitChoice, setuserLimitChoice] = useState(10);
+    const [usersDadJokes, setUsersDadJokes] = useState([]);
+    const [jokeNumberOne, setJokeNumberOne] = useState( '' );
+    const [jokeNumberTwo, setJokeNumberTwo] = useState( '' );
+    const [jokeNumberThree, setJokeNumberThree] = useState( '' );
 
-    const handleSubmit = (event) => {
-        
-        event.preventDefault();
-        // setuserLimitChoice(userLimitChoice - usersDadJokes.length);
-        // setSearchTerm(userChoice);
-        // setSubmit(true);
-        propTerm(userChoice);
-        propLimit(userLimitChoice - usersDadJokes.length);
-        propSubmit(true);
-        console.log(userChoice);
-        navigate('/results');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setuserLimitChoice(userLimitChoice - usersDadJokes.length);
+        setSearchTerm(userChoice);
+        setSubmit(true);
+        if (submit === true ) {
+            const addDadJokes = [{jokeNumberOne}, {jokeNumberTwo}, {jokeNumberThree}]
+        }
 
     }
 
@@ -32,27 +34,59 @@ const UserInputs = ({ propTerm, propLimit, propSubmit }) => {
         setUserChoice(e.target.value);
     } 
 
-    }
 
-    const userButton = () => {
-        console.log(userChoice);
-        // setSearchTerm(propTerm);
-       
-        
-    }
-    // console.log(searchTerm);
+    // const handleButtonClick = (e) => {
+    //     setRandomButton(true);
+    // }
+
+    const apiKey = `34_FRr4gH3efbjKeNMjRmPjTM8phiy64ND24X1GElr8`
 
     useEffect(() => {
+        axios({
+            url: `https://api.unsplash.com/search/photos`,
+            dataResponse: `json`,
+            method: `GET`,
+            params: {
+                client_id: apiKey,
+                query: searchTerm,
+                per_page: 10,
+            }
+        }).then((response) => {
+            setPhotos(response.data.results);
+        }).catch((error) => {
+            setApiError(error)
+        })
+    }, [searchTerm]);
 
-        return () => {
-            console.log("leaving the page");
-            console.log(userChoice);
-            handleSubmit();
-            
+
+    const randomNumber = Math.floor(Math.random() * 64);
+    
+
+    useEffect(() => {
+        if(submit){
+            axios({
+                url: `https://icanhazdadjoke.com/search`,
+                dataResponse: `json`,
+                method: `GET`,
+                headers: {
+                    "Accept": "application/json"
+                },
+                params: {
+                    limit: userLimitChoice,
+                    page: randomNumber,
+                    total_jokes: 100
+                }
+            }).then((response) => {
+                setJokes(response.data.results)
+            }).catch((error) => {
+                setApiError(error)
+            })
         }
-    }, [])
+    }, [submit])
 
-    console.log(propTerm);
+    
+    
+    
 
     return (
         <>
@@ -71,28 +105,43 @@ const UserInputs = ({ propTerm, propLimit, propSubmit }) => {
                         <option value="cats">Cats</option>
                         <option value="bunnies">Bunnies</option>
                     </select>
-                    <label htmlFor="customJoke1">Enter your dad joke:
-                    </label>
-                    <input type ="text" id="customJoke1" name="customJoke1"/>
-                    {/* <button type="submit" id="jokeButton1" name="jokeButton1"></button> */}
-                    <label htmlFor="customJoke2">
-                    Enter your dad joke:
-                    </label>
-                    <input type ="text" id="customJoke2" name="customJoke2"/>
-                    {/* <button  type="submit" id="jokeButton2" name="jokeButton2"></button> */}
-                    <label htmlFor="customJoke3">
-                        Enter your dad joke:
-                    </label>
-                    <input type ="text" id="customJoke3" name="customJoke3"/>
-                    {/* <button  type="submit" id="jokeButton3" name="jokeButton3"></button> */}
-                    <Link to='/results'  onClick={ handleSubmit }>Submit</Link>
-
+                    <div>
+                        <input
+                            type="text"
+                            value={jokeNumberOne}
+                            placeholder="Enter your dad joke"
+                            onChange={e => setJokeNumberOne(e.target.value)}
+                        />
+                        <p>
+                            <strong>{jokeNumberOne}</strong>
+                        </p>
+                        <input
+                            type="text"
+                            value={jokeNumberTwo}
+                            placeholder="Enter your dad joke"
+                            onChange={e => setJokeNumberTwo(e.target.value)}
+                        />
+                        <p>
+                            <strong>{jokeNumberTwo}</strong>
+                        </p>
+                        <input
+                            type="text"
+                            value={jokeNumberThree}
+                            placeholder="Enter your dad joke"
+                            onChange={e => setJokeNumberThree(e.target.value)}
+                        />
+                        <p>
+                            <strong>{jokeNumberThree}</strong>
+                        </p>
+                    </div>
+                    <button type="submit">Submit</button>
+                    
                 </form>
-                <button >userinputjs button</button>
             </div>
+            <Results photos={photos} jokes={jokes} />
         </>
     )
 
-
+}
 
 export default UserInputs;
