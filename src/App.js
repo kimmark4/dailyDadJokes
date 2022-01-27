52fb25531e9c1c2f73f61b334ef272710271736f
Mark <mark.kimk622@gmail.com>
2022-01-27 14:17:09 -0500

    worked on making a prompt at the end of the slide show

--- src/App.js
+++ src/App.js
@@ -12,19 +12,21 @@ function App() {
   const [submit, setSubmit] = useState(false);
   const [userLimitChoice, setuserLimitChoice] = useState(10);
   const [searchTerm, setSearchTerm] = useState('');
-  
+  const [usersJokes, setUsersJokes] = useState([]);
 
-  
 
   return (
     <div className="dadJokeApp">
 
       <Header />
       <Routes>
-        <Route path="/" element={<UserInputs propTerm={searchTerm => setSearchTerm(searchTerm)} propLimit={userLimitChoice => setuserLimitChoice(userLimitChoice)} propSubmit={submit => setSubmit(submit)} />} />
-        <Route path="/results" element={<Results submit={submit} userLimitChoice={userLimitChoice} searchTerm={searchTerm} />} />
+        <Route path="/" element={<UserInputs propTerm={searchTerm => setSearchTerm(searchTerm)} propLimit={userLimitChoice => setuserLimitChoice(userLimitChoice)} propSubmit={submit => setSubmit(submit)} propJokes={usersJokes => setUsersJokes(usersJokes)} />} />
+        <Route path="/results" element={<Results submit={submit} userLimitChoice={userLimitChoice} searchTerm={searchTerm} usersJokes={usersJokes} />} />
       </Routes>
 
+      <footer>
+        <p>Copyright © Juno College </p>
+      </footer>
     </div>
   );
 }
